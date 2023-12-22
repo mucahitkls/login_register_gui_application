162d60763db47df806269059b4187817adf13557
mücahit keleş <mchtttkls@gmail.com>
2023-12-22 23:28:15 +0100

    loggin added for db connection part

--- database/db_session.py
+++ database/db_session.py
@@ -1,3 +1,4 @@
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,9 +10,20 @@ def get_db_url():
     config.read('..\\login_gui_with_tkinter\\app.config')
     return config['database']['DB_URL']
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname) -%(message)s', filename='app.log', filemode='a')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname) -8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
 
 DB_URL = get_db_url()
+try:
+    engine = create_engine(url=DB_URL)
+    Localsession = sessionmaker(bind=engine)
+    Base = declarative_base()
+except Exception as e:
+    logging.error(f"An error occurred while setting up the database: {e}", exc_info=True)
 
-engine = create_engine(url=DB_URL)
-Localsession = sessionmaker(bind=engine)
-Base = declarative_base()
